d1f73727262281ec239ed66d6fa224f2246a6385
Welington <welingtonnej@gmail.com>
2023-11-11 12:52:39 -0300

    código desafio

---
+++ index.js
@@ -0,0 +1,27 @@
+// Variáveis
+let nomeHeroi = "SuperHerói";
+let xpHeroi = 8000;
+
+// Estrutura de decisão
+let nivelHeroi;
+
+if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+// Saída
+console.log("O Herói de nome  + {nomeHero} + " está no nível de " + {nivelHeroi}");
